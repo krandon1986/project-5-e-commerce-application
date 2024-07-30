2ad6fca599f7de6815bd37e00ad032dc26138e22
Karim_Randon <krandon1986@yahoo.co.uk>
2024-07-30 14:59:51 +0000

    Allowed host

--- hardwear/settings.py
+++ hardwear/settings.py
@@ -29,7 +29,7 @@ DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-krandon1986-project5eco-07vyxzt73ud.ws.codeinstitute-ide.net',
-    '.herokuapp.com'
+    'hardwear-1e19c988931d.herokuapp.com'
     ]
 CSRF_TRUSTED_ORIGINS = [
     'https://8000-krandon1986-project5eco-07vyxzt73ud.ws.codeinstitute-ide.net',
