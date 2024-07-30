2c30d32be6c9a2d2884c83b883a80c741e263454
Karim_Randon <krandon1986@yahoo.co.uk>
2024-07-30 19:50:04 +0000

    Adjusting host addresses

--- hardwear/settings.py
+++ hardwear/settings.py
@@ -28,14 +28,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY,' '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = [
-    '8000-krandon1986-project5eco-07vyxzt73ud.ws.codeinstitute-ide.net' 'localhost',
-    'hardwear-1e19c988931d.herokuapp.com'
-    ]
-CSRF_TRUSTED_ORIGINS = [
-    'https://8000-krandon1986-project5eco-07vyxzt73ud.ws.codeinstitute-ide.net',
-    'https://hardwear-1e19c988931d.herokuapp.com'
-    ]
+ALLOWED_HOSTS = ['8000-krandon1986-project5eco-07vyxzt73ud.ws.codeinstitute-ide.net', 'localhost', 'hardwear-1e19c988931d.herokuapp.com']
+CSRF_TRUSTED_ORIGINS = ['https://hardwear-1e19c988931d.herokuapp.com','https://8000-krandon1986-project5eco-07vyxzt73ud.ws.codeinstitute-ide.net']
 
 # Application definition
 
