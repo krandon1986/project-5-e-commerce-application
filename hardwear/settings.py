043d928c07e0ec38db105ee5fe6f2f0c15fbc383
Karim_Randon <krandon1986@yahoo.co.uk>
2024-07-30 13:12:08 +0000

    Setting up the  S3Bucket Configations in settings

--- hardwear/settings.py
+++ hardwear/settings.py
@@ -28,9 +28,12 @@ DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-krandon1986-project5eco-07vyxzt73ud.ws.codeinstitute-ide.net',
-    '.herokuapp.com'
+    'hardwear-1e19c988931d.herokuapp.com'
     ]
-CSRF_TRUSTED_ORIGINS = ['https://8000-krandon1986-project5eco-07vyxzt73ud.ws.codeinstitute-ide.net']
+CSRF_TRUSTED_ORIGINS = [
+    'https://8000-krandon1986-project5eco-07vyxzt73ud.ws.codeinstitute-ide.net',
+    'https://hardwear-1e19c988931d.herokuapp.com'
+    ]
 
 # Application definition
 
@@ -53,6 +56,7 @@ INSTALLED_APPS = [
     
     # Other
     'crispy_forms',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -177,6 +181,24 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if 'USE_AWS' in os.environ:
+    # Bucket Config
+    AWS_STORAGE_BUCKET_NAME = 'hardwear-1e19c988931d'
+    AWS_S3_REGION_NAME = 'us-east-1'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    # static and media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    # Override static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}'
 
 # Stripe
 FREE_DELIVERY_THRESHOLD = 40
