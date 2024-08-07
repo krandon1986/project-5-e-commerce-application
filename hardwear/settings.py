5260019d2e69f7233ef76e0c4292223cb76a7b7f
Karim Randon <krandon1986@yahoo.co.uk>
2024-08-07 21:34:20 +0000

    Code adjustment

--- hardwear/settings.py
+++ hardwear/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY,''vdz2#j-$ivd(+xphs#t*0_p_dt#d^f0wgq(4@+5*t1i4@&(c=k ')
+SECRET_KEY = os.environ.get('SECRET_KEY','')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False #'DEVELOPMENT' in os.environ
