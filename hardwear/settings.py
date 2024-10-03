aa356840a64f2ff1c69fcb5cf21ae3ba9c4e64d0
Karim Randon <krandon1986@yahoo.co.uk>
2024-10-03 06:07:30 +0000

    setting the debug on false

--- hardwear/settings.py
+++ hardwear/settings.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import os
 from pathlib import Path
+import os
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
